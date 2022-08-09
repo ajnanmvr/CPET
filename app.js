d7aa4af1cab4a006d3d6cd8cd22b96062850e72d
Janishnehyan03 <nehyanjanish@gmail.com>
2022-08-09 17:57:40 +0530

    process.env.PWD = process.cwd(); added

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.use(require("./utils/gloablErrors"));
 // app.all("*", (req, res, next) => {
 //   next(new AppError(`Cant find ${req.originalUrl}  on the server`, 404));
 // });
-
+process.env.PWD = process.cwd();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", function (req, res) {
