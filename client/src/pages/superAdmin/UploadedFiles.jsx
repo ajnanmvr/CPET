fbbb62f301574b9e7304e8d89967714050d06bbd
ajnanmvr <ajnanpulikkathody@gmail.com>
2022-11-23 16:07:22 +0530

    all downloads theme updated

--- client/src/pages/superAdmin/UploadedFiles.jsx
+++ client/src/pages/superAdmin/UploadedFiles.jsx
@@ -1,13 +1,23 @@
+import {
+  faFileExcel,
+  faFileImage,
+  faFilePdf,
+  faFileText,
+  faFileWord,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Axios from "../../Axios";
+import moment from 'moment'
 
 function UploadedFiles() {
   const { id } = useParams();
   const [uploads, setUploads] = useState([]);
 
+  
   const getAllUploads = async () => {
     try {
       let { data } = await Axios.get(`/uploads/${id}`);
@@ -21,7 +31,7 @@ function UploadedFiles() {
   }, []);
   return (
     <div className="overflow-x-auto relative">
-      <h1 className="text-center font-bold capitalize text-4xl my-12">
+      <h1 className="text-center font-bold uppercase text-4xl my-12">
         uploaded files
       </h1>
       <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
@@ -37,7 +47,13 @@ function UploadedFiles() {
               Branch Code
             </th>
             <th scope="col" className="py-3 px-6">
-              File
+              Download File
+            </th>
+            <th scope="col" className="py-3 px-6">
+              File Type
+            </th>
+            <th scope="col" className="py-3 px-6">
+             Uploaded Time
             </th>
           </tr>
         </thead>
@@ -53,16 +69,72 @@ function UploadedFiles() {
                 </th>
                 <td className="py-4 px-6">{upload?.uploadedBy?.branchName}</td>
                 <td className="py-4 px-6">{upload?.uploadedBy?.branchCode}</td>
+
                 <td className="py-4 px-6">
-                  {" "}
                   <a
                     target={"_blank"}
                     href={`/${upload.fileName}`}
-                    className="bg-[#333] px-3 py-2 font-bold text-white  hover:bg-gray-500"
+                    className="bg-[#333] rounded-md px-3 py-2 font-bold text-white  hover:bg-gray-500"
                   >
-                    View
+                    Download
                   </a>
                 </td>
+                <td className="py-4 px-6">
+                  <p>
+                    {upload.fileName?.split(".").pop() == "pdf" ? (
+                      <FontAwesomeIcon
+                        className="text-3xl text-red-400"
+                        icon={faFilePdf}
+                      />
+                    ) : (
+                      ""
+                    )}
+                    {upload.fileName?.split(".").pop() == "xlsx" ||
+                    upload.fileName?.split(".").pop() == "csv" ||
+                    upload.fileName?.split(".").pop() == "xlsm" ? (
+                      <FontAwesomeIcon
+                        className="text-3xl text-green-400"
+                        icon={faFileExcel}
+                      />
+                    ) : (
+                      ""
+                    )}
+                    {upload.fileName?.split(".").pop() == "doc" ||
+                    upload.fileName?.split(".").pop() == "docs" ? (
+                      <FontAwesomeIcon
+                        className="text-3xl text-blue-400"
+                        icon={faFileWord}
+                      />
+                    ) : (
+                      ""
+                    )}
+                    {upload.fileName?.split(".").pop() == "txt" ? (
+                      <FontAwesomeIcon
+                        className="text-3xl text-gray-400"
+                        icon={faFileText}
+                      />
+                    ) : (
+                      ""
+                    )}
+                    {upload.fileName?.split(".").pop() == "png" ||
+                    upload.fileName?.split(".").pop() == "jpg" ||
+                    upload.fileName?.split(".").pop() == "jpeg" ? (
+                      <FontAwesomeIcon
+                        className="text-3xl text-violet-400"
+                        icon={faFileImage}
+                      />
+                    ) : (
+                      ""
+                    )}
+                  </p>
+                  .{upload.fileName?.split(".").pop()}
+                </td>
+                <td className="py-4 px-6">
+                  <p className="text-green-500">{moment(upload?.createdAt).format('HH:MM A')}</p>
+                  <p>{moment(upload?.createdAt).format('DD-MM-YY')}</p>
+                  <p>{moment(upload?.createdAt).format('ddd')}</p>
+                </td>
+
               </tr>
             ))}
         </tbody>
