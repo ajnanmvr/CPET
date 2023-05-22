3c5e21d3257dda40641b52f07d65e7c138affb57
Janishnehyan03 <nehyanjanish@gmail.com>
2023-05-22 17:28:07 +0530

    phone number added

--- client/src/pages/AdmissionStarted.jsx
+++ client/src/pages/AdmissionStarted.jsx
@@ -114,7 +114,6 @@ function AdmissionStarted() {
                   <option hidden>Select </option>
                   {DISTRICT.map((district, index) => (
                     <>
-                      {" "}
                       <option key={index} value={district}>
                         {district}
                       </option>
@@ -128,6 +127,9 @@ function AdmissionStarted() {
                     <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                       <p className="mb-2  font-semibold tracking-tight text-teal-700 dark:text-white">
                         {item?.branchName}
+                      </p>
+                      <p className="font-normal text-gray-700 dark:text-gray-400">
+                        {item?.phone}
                       </p>
                       <p className="font-normal text-gray-700 dark:text-gray-400">
                         {item?.district}
